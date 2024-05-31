d2c2b900f94f9d2317bd48ddb995e8d7a28c3b26
ZaraStudie <zahrastudie@gmail.com>
2024-05-31 22:04:37 +0200

    Update csvtojson.py

--- csvtojson.py
+++ csvtojson.py
@@ -4,20 +4,24 @@ import json
 csv_file = 'profiles1.csv'
 json_file = 'data.json'
 
-data = []
+def convert_csv_to_json(csv_file, json_file):
+    data = []
 
-# Read the CSV file
-try:
-    with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-except Exception as e:
-    print("Error reading CSV file:", e)
+  
+    try:
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
+    except Exception as e:
+        print("Error reading CSV file:", e)
 
-# Write the JSON file
-try:
-    with open(json_file, 'w') as file:
-        json.dump(data, file, indent=4)
-except Exception as e:
-    print("Error writing JSON file:", e)
+ 
+    try:
+        with open(json_file, 'w') as file:
+            json.dump(data, file, indent=4)
+    except Exception as e:
+        print("Error writing JSON file:", e)
+
+if __name__ == '__main__':
+    convert_csv_to_json(csv_file, json_file)
